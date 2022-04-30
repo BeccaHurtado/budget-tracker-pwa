2ffb74e0e1268b042c2752c5c5dd9c0fe351a954
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-04-30 10:33:26 -0700

    adding to service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,9 +1,10 @@
 const APP_PREFIX = 'FoodFest-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE = "datacache-"+VERSION;
 
 const FILES_TO_CACHE = [
-    
+    "/",
     "../index.html",
     "../css/styles.css",
     "../icons/icon-72x72.png",
@@ -28,4 +29,33 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+})
+
+self.addEventListener('fetch', function(e) {
+    if (e.request.url.includes('/api/') ){
+        e.respondWith(
+            caches.open(DATA_CACHE)
+            .then(cachedData =>{
+                return fetch(e.request).then(result => {
+                    if(result.status == 200){
+                        cachedData.put(e.request.url, result.clone())
+                    }
+                    return result
+                })
+            }).catch(err =>{
+                return cachedData.match(e.result);
+            })
+        )
+        return; 
+    }
+    e.respondWith(fetch(e.request).catch(function(){
+        return caches.match(e.request).then(function(response) {
+            if(response) {
+                return response;
+            } else if (e.request.headers.get('accept').includes('text/html')){
+                return caches.match('/')
+            }      
+
+        })
+    }))
 })
