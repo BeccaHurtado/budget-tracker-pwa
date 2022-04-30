7e851aeccc6ec227aa88593acc6814d80af9a2c8
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-04-30 10:10:06 -0700

    caching all files

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -3,19 +3,19 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/",
-    "./index.html",
-    "./css/styles.css",
-    "./icon-72x72.png",
-    "./icon-96x96.png",
-    "./icon-128x128.png",
-    "./icon-144x144.png",
-    "./icon-152x152.png",
-    "./icon-192x192.png",
-    "./icon-384x384.png",
-    "./icon-512x512.png",
-    "./js/index.js",
-    "./js/manifest.json",
+    
+    "../index.html",
+    "../css/styles.css",
+    "../icons/icon-72x72.png",
+    "../icons/icon-96x96.png",
+    "../icons/icon-128x128.png",
+    "../icons/icon-144x144.png",
+    "../icons/icon-152x152.png",
+    "../icons/icon-192x192.png",
+    "../icons/icon-384x384.png",
+    "../icons/icon-512x512.png",
+    "../js/index.js",
+    "../js/manifest.json",
 
     
 ];
